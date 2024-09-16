2eb11b28a2c6ab55b779493f06e0855f96fb9f3a
Deepak Kharah <42672761+Deepak-Kharah@users.noreply.github.com>
2024-09-16 22:36:46 +0530

    chore: set up google tags with partytown

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { partytownVite } from "@builder.io/partytown/utils";
+import { join } from "path";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [
+    solid(),
+    partytownVite({
+      dest: join(__dirname, "dist", "~partytown"),
+    }),
+  ],
+});
